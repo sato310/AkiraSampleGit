17579c8381d86abcd39a432cc1ea51897c627674
akira000041 <akira000041@gmail.com>
2013-12-19 20:12:50 +0900

    練習用のコメントを追加しました

--- src/com/sato310/akirasamplegite/MainActivity.java
+++ src/com/sato310/akirasamplegite/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+/**
+ * ‚±‚ê‚ÍGit‚Ì‚½‚ß‚ÌƒRƒƒ“ƒg‚Å‚·B
+ * @author Owner
+ *
+ */
 public class MainActivity extends Activity {
 
 	@Override
