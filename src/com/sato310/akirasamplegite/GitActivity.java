ee4612cf3b96730246964f600f34828b321dd2d2
akira000041 <akira000041@gmail.com>
2013-12-19 20:44:22 +0900

    アクティビティー追加

--- src/com/sato310/akirasamplegite/GitActivity.java
+++ src/com/sato310/akirasamplegite/GitActivity.java
@@ -1,7 +1,23 @@
 package com.sato310.akirasamplegite;
 
+import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
 
 public class GitActivity extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.git, menu);
+        return true;
+    }
+    
 }
